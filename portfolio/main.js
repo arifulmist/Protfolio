d528828794803b8d0a2dc8c63f82289acd59f6bf
Md Ariful Islam <154077378+arifulmist@users.noreply.github.com>
2025-05-01 10:55:45 +0600

    Delete portfolio directory

--- portfolio/main.js
+++
@@ -1,7 +0,0 @@
-var typed= new Typed('#ele',{
-     strings: ['Fronted Developer...' , 'Web Developer ...' , 'CSE Engineer ...'],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop:true
-});
